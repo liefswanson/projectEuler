7abee8bd05d4f3e602fdadd8f5fb802dfabe703e
liefswanson <liefs@sfu.ca>
2014-10-10 16:08:42 -0700

    restricted the fib function (generates an infinite list of fibonnaci numbers) to only output integers, as opposed to integrals. This removed a huge amount of memory usage as haskell had to generalize the data type (It would not resolve). Trying to generate the 40th fibonnaci number would take up more than 20 gigs of ram... Now it is instantaneous.

--- 002/Fib.hs
+++ 002/Fib.hs
@@ -1,28 +1,32 @@
-module Fib where
+--module Fib where
+
+main :: IO ()
+main = print (take 40 fibs)
 
 -- bad example of a function which generates fibonacci numbers
 -- a really bad example
-dumdFib :: Integral a => a -> a
+dumdFib :: Integer -> Integer
 dumdFib 0 = 0
 dumdFib 1 = 1
 dumdFib n = dumdFib(n-1) + dumdFib(n-2)
 
 
 -- Binet's formula for fibonacci numbers
-fib :: Integral a => a -> a
+fib :: Integer -> Integer
 fib n = round $ phi ** fromIntegral n / sq5
   where
     sq5 = sqrt 5 :: Double
     phi = (1+sq5) / 2  
 
 -- generates an infinite list of primes
-fibs :: Integral a => [a]
+fibs :: [Integer]
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
 
 -- generates an infinite list of primes... much faster, like much faster
-fibs' :: Integral a => [a]
+fibs' :: [Integer]
 fibs' = map fst $ iterate (\(a,b) -> (b,a+b)) (1,2)
 
 -- might be a smarter way to pull the correct number of places from fibs'?
-solution :: Integral a => a -> a
+solution :: Integer -> Integer
 solution n = sum . filter even . takeWhile (< n) $ fibs'
+
